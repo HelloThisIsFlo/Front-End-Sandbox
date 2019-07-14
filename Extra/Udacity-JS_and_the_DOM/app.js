7b86d226d8a34c96f5f1682790d148dddef8ebbe
Florian Kempenich <Flori@nKempenich.com>
2019-07-14 15:48:51 +0100

    Experiment with Performance benchmarking

--- Extra/Udacity-JS_and_the_DOM/app.js
+++ Extra/Udacity-JS_and_the_DOM/app.js
@@ -1,15 +1,108 @@
-
-const mainHeading = document.querySelector('h1');
-
-
-document.addEventListener('click', event => {
-    mainHeading.style.backgroundColor = 'red';
+function logAllClickPosition() {
+  document.addEventListener("click", event => {
     console.log(event);
-    console.log('event.clientX :', event.clientX);
-    console.log('event.clientY :', event.clientY);
-    console.log('event.screenX :', event.screenX);
-    console.log('event.screenY :', event.screenY);
-    console.log('event.pageX :', event.pageX);
-    console.log('event.pageY :', event.pageY);
+    console.log("event.clientX :", event.clientX);
+    console.log("event.clientY :", event.clientY);
+    console.log("event.screenX :", event.screenX);
+    console.log("event.screenY :", event.screenY);
+    console.log("event.pageX :", event.pageX);
+    console.log("event.pageY :", event.pageY);
+  });
+}
 
-})
+function paragraphClickListener(event) {
+  if (event.target.nodeName === "P") {
+    let position = Number(event.target.getAttribute("data-position"));
+    console.log(`paragraph #${position} was clicked`);
+  }
+}
+function buildContainingDiv() {
+  const paragraphsDiv = document.createElement("div");
+  paragraphsDiv.addEventListener("click", paragraphClickListener);
+  paragraphsDiv.classList.add("paragraphs");
+  return paragraphsDiv;
+}
+function buildParagraphTemplate() {
+  return document.createElement("p");
+}
+function buildParagraphFromTemplate(template, index) {
+  const paragraph = template.cloneNode();
+  paragraph.textContent = `This is paragraph #${index}`;
+  paragraph.setAttribute("data-position", index);
+  return paragraph;
+}
+
+const params = {
+  paragraphsPerGroup: 100,
+  triesForEachVersion: 1000
+};
+
+const versionsToTest = {
+  creatingNewElementEachTime() {
+    const allParagraphsDiv = buildContainingDiv();
+
+    const paragraphs = new Array();
+    for (let i = 0; i < params.paragraphsPerGroup; ) {
+      const paragraph = document.createElement("p");
+      paragraph.textContent = `This is paragraph #${++i}`;
+      paragraph.setAttribute("data-position", i);
+      paragraphs.push(paragraph);
+    }
+
+    allParagraphsDiv.append(...paragraphs);
+    document.body.append(allParagraphsDiv);
+  },
+
+  cloneTemplateAndAppendAllAtOnce() {
+    const allParagraphsDiv = buildContainingDiv();
+    const template = buildParagraphTemplate();
+    const paragraphs = [];
+
+    for (let i = 0; i < params.paragraphsPerGroup; ) {
+      paragraphs.push(buildParagraphFromTemplate(template, ++i));
+    }
+
+    allParagraphsDiv.append(...paragraphs);
+    document.body.append(allParagraphsDiv);
+  },
+
+  cloneTemplateAndAppendInTheLoop() {
+    const allParagraphsDiv = buildContainingDiv();
+    const template = buildParagraphTemplate();
+    for (let i = 0; i < params.paragraphsPerGroup; ) {
+      allParagraphsDiv.appendChild(buildParagraphFromTemplate(template, ++i));
+    }
+    document.body.append(allParagraphsDiv);
+  }
+};
+
+function clean() {
+  const paragraphDivs = [];
+  for (p of document.getElementsByClassName("paragraphs")) {
+    paragraphDivs.push(p);
+  }
+  for (paragraphDiv of paragraphDivs) {
+    paragraphDiv.remove();
+  }
+  console.log(`Cleaned ${paragraphDivs.length} groups of paragraphs`);
+}
+
+function testAllVersions() {
+  for (version in versionsToTest) {
+    let durationSum = 0;
+    for (let i = 0; i < params.triesForEachVersion; i++) {
+      const start = performance.now();
+      versionsToTest[version]();
+      const end = performance.now();
+
+      durationSum += end - start;
+    }
+
+    const avgDuration = durationSum / params.triesForEachVersion;
+    console.log(`${version}\n-> ${avgDuration}`);
+  }
+
+  clean();
+}
+
+testAllVersions();
